9f16f7346d0427d07ea938a8bcb1949c614c0435
Alejandro Solorzano <juan.solorzanog@upb.edu.co>
2025-08-26 17:36:03 -0500

    Se realizan ejercicios en clase en el archivo ejercicio3.py

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -1,5 +1,5 @@
-#nombre = input("Por favor, ingrese su nombre: ")
-#print("Bienvenido ", nombre)
+nombre = input("Por favor, ingrese su nombre: ")
+print("Bienvenido ", nombre)
 
 # Calcular el IMC (Índice de Masa Corporal): peso / estatura^2
 print("CALCULADORA DE INDICE DE MASA CORPORAL")
@@ -9,15 +9,18 @@ peso = float(input("Ingrese su peso (kg): "))
 
 IMC = peso / estatura ** 2
 print("Su IMC es: ", IMC)
+
 if IMC < 18.49:
-    print("Alerta: USTED tiene un bajo peso corporal")
-elif 18.5 < IMC < 24.99:
-    print("Su peso corporal es normal")
-elif 25 < IMC < 29.9:
-    print("Alerta: USTED tiene sobrepeso")
-elif 30 < IMC < 39.99:
-    print("Alerta: USTED tiene obesidad")
+    msg = "bajo peso corporal"
+elif IMC < 25:
+    msg = "normal"
+elif IMC < 30:
+    msg = "sobrepeso"
+elif IMC < 40:
+    msg = "obesidad"
 else:
-    print("Alerta: USTED tiene obesidad EXTREMA")
+    msg = "obesidad EXTREMA"
+
+print(f"{nombre}, su IMC es {IMC:0.2f} y su condición corporal es {msg}.")
 
 
