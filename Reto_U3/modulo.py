916d8c79f8e12e854afd1fd7c029559c26e15ecb
Sga-9762 <juanalejo7710@gmail.com>
2025-09-17 23:10:44 -0500

    Se actualiza el código del módulo para incorporar nuevas funciones en el simulador de alertas de velocidad.

--- Reto_U3/modulo.py
+++ Reto_U3/modulo.py
@@ -1,17 +1,18 @@
 import random
 
-OAT = 0
-IAS = 0 
-FLAPS = 0
-LDG_GEAR = 0
+#OAT = 0
+#IAS = 0 
+#FLAPS = 0
+#LDG_GEAR = 0
+#FLAP_CFG = 0
 
-def valuecomputer(OAT, IAS, FLAPS, LDG_GEAR):
+def valuecomputer(OAT, IAS, FLAPS, LDG_GEAR, FLAP_CFG):
             OAT = random.randrange(-56, 40)
-            IAS = random.randrange(0, 500)
+            IAS = random.randrange(0, 410)
             FLAPS = random.randrange(0,2)
             LDG_GEAR = random.randrange(0,2)
             FLAP_CFG = random.randrange(0,5)
-            return OAT, IAS, FLAPS, LDG_GEAR
+            return OAT, IAS, FLAPS, LDG_GEAR, FLAP_CFG
 
 def Ascenso():
     pass
@@ -34,28 +35,40 @@ def SpdAlert():
     LDG_GEAR = 0 
     t_vuelo = int(input("Ingrese el tiempo total de vuelo (en minutos): "))
     FLG_TIME = t_vuelo*60 # tiempo en segundos
-    while FLG_TIME > 0 and i <= FLG_TIME:
-       # valuecomputer()
-        OAT, IAS, FLAPS, LDG_GEAR = valuecomputer(OAT, IAS, FLAPS, LDG_GEAR)
-        print(f"iteracion: {i}", OAT, IAS, FLAPS, LDG_GEAR)
-        EC_MACH = MACH_0DG + (SPD_SD_decrease*OAT)
-        MAX_IAS = EC_MACH*ms_to_kt
-        if FLAPS == 1:
-            if FLAP_CFG == 1:
-                MAX_IAS = 238
-            elif FLAP_CFG == 2:
-                MAX_IAS = 215
-            elif FLAP_CFG == 3:
-                MAX_IAS = 195
-            elif FLAP_CFG == 4:
-                MAX_IAS = 186
+    chc = int(input("¿Iniciar el vuelo?\n 1. SI\n 2. NO\n SU SELECCION: "))
+    match chc:
+        case 1:
+            while FLG_TIME > 0 and i <= FLG_TIME:
+            # valuecomputer()
+                OAT, IAS, FLAPS, LDG_GEAR, FLAP_CFG = valuecomputer(OAT, IAS, FLAPS, LDG_GEAR, FLAP_CFG)
+                print(f"**Iteracion: {i}**\n **Temperatura externa: {OAT}°C**\n **Velocidad indicada: {IAS}kts**\n **Estado de flaps: {FLAPS}**\n **Configuracion de flaps: {FLAP_CFG}**\n **Estado del tren de aterrizaje: {LDG_GEAR}**")
+                EC_MACH = MACH_0DG + (SPD_SD_decrease*OAT)
+                MAX_IAS = EC_MACH*ms_to_kt
+                if FLAPS == 1:
+                    if FLAP_CFG == 1:
+                        #print("FLAPS 1")
+                        MAX_IAS = 238
+                    elif FLAP_CFG == 2:
+                        #print("FLAPS 2")
+                        MAX_IAS = 215
+                    elif FLAP_CFG == 3:
+                        #print("FLAPS 3")
+                        MAX_IAS = 195
+                    elif FLAP_CFG == 4:
+                        #print("FLAPS 4")
+                        MAX_IAS = 186
 
-        if LDG_GEAR == 1:
-            MAX_IAS = 250
-        if IAS >= MAX_IAS:
-            SPD_ALERT =+ 1
-            Warning(f"¡ALERTA: se ha superado la velocidad MÁXIMA de operación! ¡Reduzca la velocidad inmediatamente! Velocidad registrada: {IAS}kts")
-        i += 1
-        
-    print(f"El vuelo ha finalizado. Se generaron {SPD_ALERT} alertas durante un tiempo de vuelo de {t_vuelo} minutos, en el cual se realizaron {i} iteraciones de control.")
+                if LDG_GEAR == 1:
+                    MAX_IAS = 250
+                if IAS >= MAX_IAS:
+                    print(f"¡ALERTA: se ha superado la velocidad MÁXIMA de operación! ¡Reduzca la velocidad inmediatamente!\n Velocidad máxima: {MAX_IAS}kts \n Velocidad registrada: {IAS}kts")
+                    SPD_ALERT += 1
+                i += 1
+                
+            print(f"El vuelo ha finalizado. Se generaron {SPD_ALERT} alertas durante un tiempo de vuelo de {t_vuelo} minutos, en el cual se realizaron {i} iteraciones de control.")
+        case 2:
+            chc = int(input("¿Iniciar el vuelo?\n 1. SI\n 2. NO\n SU SELECCION: "))
+        case _:
+              print("El valor ingresado no es válido")
+              chc = int(input("¿Iniciar el vuelo?\n 1. SI\n 2. NO\n SU SELECCION: "))
     return SPD_ALERT
