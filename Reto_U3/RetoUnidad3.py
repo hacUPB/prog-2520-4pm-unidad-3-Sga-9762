61b1a08877b290b507bc1eb0ba7a6982915804d3
Sga-9762 <juanalejo7710@gmail.com>
2025-09-17 23:09:52 -0500

    Se actualiza el código del menú de selección.
    Se realiza un pequeño cambio respecto a la ubicación de la variable 'choice' en el código.

--- Reto_U3/RetoUnidad3.py
+++ Reto_U3/RetoUnidad3.py
@@ -4,9 +4,10 @@ from modulo import *
 
 
 
-choice = int(input("**¡Bienvenido!** Escoja una de las opciones a continuación:\n1. Simulador de Ascenso y Control de Vuelo\n2. Simulador de Gestión del Centro de Gravedad\n3. Sistema de Alerta de Velocidad\n4. Salir\n SU SELECCION: "))
+
 
 while True:
+    choice = int(input("**¡Bienvenido!** Escoja una de las opciones a continuación:\n1. Simulador de Ascenso y Control de Vuelo\n2. Simulador de Gestión del Centro de Gravedad\n3. Sistema de Alerta de Velocidad\n4. Salir\n SU SELECCION: "))
     match choice:
         case 1:
             pass
